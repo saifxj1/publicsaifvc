816d86f861db899b4c09959141d53cdee6db65a9
saifxj1 <saifyaset@gmail.com>
2024-11-21 20:05:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 var listener = app.listen(process.env.PORT || 2000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.listen(() => console.log("I'm Ready To Work For Saif, The Goat..! 24H"));
 app.get('/', (req, res) => {
   res.send(`
   <body>
